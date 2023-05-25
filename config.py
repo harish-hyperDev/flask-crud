ba86aecd01474cc47619dc981de900220d347e3c
harish-hyperDev <coolprogrammer.777@gmail.com>
2023-05-25 17:20:39 +0530

    creating boilerplate

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__name__))
+
+class Config:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'flask-secure-key=skdjfi1j091283:=j12/j9jdjfjkjs92139'
